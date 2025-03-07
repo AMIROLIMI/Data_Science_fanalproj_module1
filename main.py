3b70bbbbf91c65c402b651306e8c645ab9deb088
AMIROLIMI <oamir3867@mail.ru>
2025-03-07 11:06:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -43,14 +43,17 @@ def load_data(url):
 data_raw = load_data("https://github.com/AMIROLIMI/Data_Science_fanalproj_module1/raw/main/Obesity%20prediction.csv")
 st.write("### Данные до изменения")
 st.dataframe(data_raw.head())
-st.image("https://github.com/AMIROLIMI/Data_Science_fanalproj_module1/blob/main/CORR_before.png", caption="Описание изображения", use_column_width=True)
+st.image("https://raw.githubusercontent.com/AMIROLIMI/Data_Science_fanalproj_module1/main/CORR_before.png", 
+         caption="Описание изображения", use_column_width=True)
+
 
 # Загрузка обработанных данных
 data = load_data("https://github.com/AMIROLIMI/Data_Science_fanalproj_module1/raw/main/Encoded%20Standardized%20Obesity%20prediction.csv")
 data = data.drop(columns=["Unnamed: 0"])
 st.write("### Данные после изменения")
 st.dataframe(data.head())
-st.image("https://github.com/AMIROLIMI/Data_Science_fanalproj_module1/blob/main/CORR_after.png", caption="Описание изображения", use_column_width=True)
+st.image("https://raw.githubusercontent.com/AMIROLIMI/Data_Science_fanalproj_module1/main/CORR_after.png", 
+         caption="Описание изображения", use_column_width=True)
 
 # Разделение данных
 X = data.drop(columns=["Obesity"])
